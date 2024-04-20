f20eb30285ae90a78d2ded0c7b6f088fa9f231f6
Andreas Blokkum Olborg <81119964+andreasolborg@users.noreply.github.com>
2024-04-20 14:38:54 +1000

    12334

--- irooftopagility/src/main/java/net/runelite/client/plugins/irooftopagility/iRooftopAgilityPlugin.java
+++ irooftopagility/src/main/java/net/runelite/client/plugins/irooftopagility/iRooftopAgilityPlugin.java
@@ -42,7 +42,6 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDependency;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.PluginManager;
-import net.unethicalite.api.movement.Movement;
 import net.runelite.client.plugins.iutils.*;
 import net.runelite.client.plugins.iutils.scripts.ReflectBreakHandler;
 import net.runelite.client.ui.overlay.OverlayManager;
@@ -537,12 +536,7 @@ public class iRooftopAgilityPlugin extends Plugin {
                 return;
             }
             marksPerHour = (int) getMarksPH();
-            if (!Movement.isRunEnabled() && (Movement.getRunEnergy() >= Rand.nextInt(MIN_ENERGY, MAX_MIN_ENERGY) || (local.getHealthScale() > -1 && Movement.getRunEnergy() > 0)))
-            {
-                Movement.toggleRun();
-                Time.sleepUntil(Movement::isRunEnabled, 2000);
-                return true;
-            }
+
             state = getState();
             beforeLoc = client.getLocalPlayer().getLocalLocation();
             switch (state) {
